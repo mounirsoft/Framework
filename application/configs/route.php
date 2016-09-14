f36babd661b7fce85b2fd55f831144ccf52f29df
MounirSoft <mounirsoft.dev@gmail.com>
2016-09-14 17:03:03 +0000

    work

--- application/configs/route.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    '/blog/(\w+)/(\d+)' => ['controller' => 'index','action'=>'index']
-];
-
-//$this->router->get('/', 'HomeController@index');
-//$this->router->get('/example', 'HomeController@example');
--- library/MounirSoft/Container/IBundle.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace MounirSoft\Container;
-
-interface IBundle {
-    public function register(\MounirSoft\Container $container);
-}
